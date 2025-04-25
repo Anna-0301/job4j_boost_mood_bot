b83bc01642eabee6606060555e28fd9f2e5621e8
Anna-0301 <annabogd123@gmail.com>
2025-04-25 14:15:15 +0500

    Модели данных [#505302]

--- src/main/java/ru/job4j/bmb/repository/AchievementRepository.java
+++ src/main/java/ru/job4j/bmb/repository/AchievementRepository.java
@@ -1,7 +1,11 @@
 package ru.job4j.bmb.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import ru.job4j.bmb.model.Achievement;
+import java.util.List;
 
 @Repository
-public class AchievementRepository {
+public interface AchievementRepository extends CrudRepository<Achievement, Long> {
+    List<Achievement> findAll();
 }
