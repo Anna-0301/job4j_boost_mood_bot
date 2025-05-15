440ad8b49e75d8cb1ee22489bf84d1058ccceb7d
Anna-0301 <annabogd123@gmail.com>
2025-05-15 15:34:57 +0500

    ---

--- src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
+++ src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
@@ -1,13 +1,13 @@
 package ru.job4j.bmb;
 
 import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.repository.UserRepository;
 import ru.job4j.bmb.services.BotCommandHandler;
+import ru.job4j.bmb.services.MoodService;
 import ru.job4j.bmb.services.TelegramBotService;
+import ru.job4j.bmb.services.TgUI;
 
 public class DIByDirectInjectMain {
     public static void main(String[] args) {
-        var handler = new BotCommandHandler();
-        var tg = new TelegramBotService(handler);
-        tg.receive(new Content(null));
     }
 }
--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -1,9 +1,29 @@
 package ru.job4j;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.services.MoodService;
+
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class MainTest {
+    private final DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern("dd-MM-yyyy HH:mm")
+            .withZone(ZoneId.systemDefault());
+
+    static final long SECOND = 1000;
+    static final long MINUTE = SECOND * 60;
+    static final long HOUR = MINUTE * 60;
+    static final long DAY = HOUR * 24;
+    static final long MONTH = DAY * 30;
+
     @Test
     public void whenActionThenResult() {
         int excepted = 1;
